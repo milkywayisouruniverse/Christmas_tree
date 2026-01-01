a30d1ac1d42de5cc81787811f9c0138e792fd60c
milkywayisouruniverse <dazzlingphoenix2022@gmail.com>
2026-01-01 20:19:29 +0530

    Add marigold turtle drawing

--- Christmas_tree.py
+++ Christmas_tree.py
@@ -1,19 +1,52 @@
-def draw_christmas_tree(height):
-    # Star
-    print(" " * height + "⭐")
+import turtle
+import random
+import math
 
-    # Tree body
-    for i in range(height):
-        spaces = height - i
-        stars = 2 * i + 1
-        print(" " * spaces + "*" * stars)
+# Screen setup
+screen = turtle.Screen()
+screen.bgcolor("skyblue")
 
-    # Trunk
-    trunk_width = 3
-    trunk_height = 2
-    for _ in range(trunk_height):
-        print(" " * (height - 1) + "|" * trunk_width)
+flower = turtle.Turtle()
+flower.speed(0)
+flower.hideturtle()
 
+# Draw stem
+flower.penup()
+flower.goto(0, -250)
+flower.setheading(90)
+flower.pendown()
+flower.color("green")
+flower.width(6)
+flower.forward(200)
 
-# Call the function
-draw_christmas_tree(6)
+# Move to flower center
+flower.penup()
+flower.goto(0, -50)
+flower.pendown()
+
+# Draw dandelion seeds
+flower.color("white")
+flower.width(2)
+
+for angle in range(0, 360, 6):
+    flower.penup()
+    flower.setheading(angle)
+    flower.forward(10)
+    flower.pendown()
+    flower.forward(120)
+
+    # Seed head
+    flower.penup()
+    flower.forward(5)
+    flower.dot(4)
+
+    flower.backward(135)
+
+# Center
+flower.penup()
+flower.goto(0, -50)
+flower.pendown()
+flower.color("yellow")
+flower.dot(20)
+
+screen.mainloop()
